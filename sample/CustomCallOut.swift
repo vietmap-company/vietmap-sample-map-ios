a060474f57bcb1f1b0ffd09694aed0a2a47518d6
thanhdang198 <dangtrongthanh1998@gmail.com>
2024-05-06 15:49:32 +0700

    init project

---
+++ sample/CustomCallOut.swift
@@ -0,0 +1,142 @@
+import Foundation
+import VietMap
+
+class CustomCalloutView: UIView, MGLCalloutView {
+
+    weak var delegate: MGLCalloutViewDelegate?
+    
+    var representedObject: MGLAnnotation = MGLPointAnnotation()
+    var leftAccessoryView: UIView = UIView()
+    var rightAccessoryView: UIView = UIView()
+    
+    // Move annotation when user move the map
+    var isAnchoredToAnnotation = true
+    
+    // Dismiss the annotation when user move the map
+    var dismissesAutomatically = false
+
+    //MARK: Subviews -
+    let titleLabel:UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.boldSystemFont(ofSize: 14.0)
+        return label
+    }()
+
+    let subtitleLabel:UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.boldSystemFont(ofSize: 11.0)
+        return label
+    }()
+
+    let imageView:UIImageView = {
+        let imageview = UIImageView(frame: CGRect(x: 0, y: 0, width: 12, height: 12))
+        imageview.translatesAutoresizingMaskIntoConstraints = false
+        imageview.contentMode = .scaleAspectFit
+        return imageview
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .clear
+//        titleLabel.backgroundColor = .clear
+//        addSubview(titleLabel)
+        setup()
+    }
+
+    required init?(coder decoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setup() {
+           // setup this view's properties
+       self.backgroundColor = UIColor.white
+
+       // And their Subviews
+       self.addSubview(titleLabel)
+       self.addSubview(subtitleLabel)
+       self.addSubview(imageView)
+
+       // Add Constraints to subviews
+       let spacing:CGFloat = 8.0
+
+       imageView.topAnchor.constraint(equalTo: self.topAnchor, constant: spacing).isActive = true
+       imageView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: spacing).isActive = true
+       imageView.heightAnchor.constraint(equalToConstant: 52.0).isActive = true
+       imageView.widthAnchor.constraint(equalToConstant: 52.0).isActive = true
+
+       titleLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: spacing).isActive = true
+       titleLabel.leftAnchor.constraint(equalTo: self.imageView.rightAnchor, constant: spacing * 2).isActive = true
+       titleLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -spacing).isActive = true
+       titleLabel.heightAnchor.constraint(equalToConstant: 50.0).isActive = true
+
+       subtitleLabel.topAnchor.constraint(equalTo: self.titleLabel.bottomAnchor, constant: spacing).isActive = true
+       subtitleLabel.leftAnchor.constraint(equalTo: self.leftAnchor, constant: spacing).isActive = true
+       subtitleLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -spacing).isActive = true
+       subtitleLabel.heightAnchor.constraint(equalToConstant: 20.0).isActive = true
+   }
+
+    func presentCallout(from rect: CGRect, in view: UIView, constrainedTo constrainedRect: CGRect, animated: Bool) {
+        if let delegate = delegate, delegate.responds(to: #selector(delegate.calloutViewWillAppear(_:))) {
+            delegate.perform(#selector(delegate.calloutViewWillAppear(_:)), with: self)
+        }
+        self.center = view.center.applying(CGAffineTransform(translationX: 0, y: -self.frame.height))
+        view.addSubview(self)
+        imageView.image = UIImage(systemName: "car")!
+        if let title = representedObject.title, let subTitle = representedObject.subtitle {
+            titleLabel.text = title
+            subtitleLabel.text = subTitle
+        }
+        
+        let frameWidth = titleLabel.bounds.size.width
+        let frameHeight = titleLabel.bounds.size.height * 2.0
+        let frameOriginX = rect.origin.x + (rect.size.width/2.0) - (frameWidth/2.0)
+        let frameOriginY = rect.origin.y - frameHeight
+        frame = CGRect(x: frameOriginX, y: frameOriginY, width: 120, height: 80)
+
+        if let delegate = delegate, delegate.responds(to: #selector(delegate.calloutViewDidAppear(_:))) {
+            delegate.perform(#selector(delegate.calloutViewDidAppear(_:)), with: self)
+        }
+    }
+
+    override var center: CGPoint {
+        didSet {
+            var newCenter = center
+            newCenter.y -= bounds.midY
+            super.center = newCenter
+        }
+    }
+    
+    func dismissCallout(animated: Bool) {
+        if superview != nil {
+                   removeFromSuperview()
+               }
+    }
+
+    // MARK: - Internals
+
+    override func draw(_ rect: CGRect) {
+        let fillColor = UIColor.white
+        let tipHeight: CGFloat = 10.0
+        let tipWidth: CGFloat = 10.0
+
+        let tipLeft = rect.origin.x + (rect.size.width / 2.0) - (tipWidth / 2.0)
+        let tipBottom = CGPoint(x: rect.origin.x + (rect.size.width / 2.0), y: rect.origin.y + rect.size.height)
+        let heightWithoutTip = rect.size.height - tipHeight
+
+        let tipPath = UIBezierPath()
+        tipPath.move(to: CGPoint(x: 0, y: 0))
+        tipPath.addLine(to: CGPoint(x: 0, y: heightWithoutTip))
+        tipPath.addLine(to: CGPoint(x: tipLeft, y: heightWithoutTip))
+        tipPath.addLine(to: tipBottom)
+        tipPath.addLine(to: CGPoint(x: tipLeft + tipWidth, y: heightWithoutTip))
+        tipPath.addLine(to: CGPoint(x: rect.width, y: heightWithoutTip))
+        tipPath.addLine(to: CGPoint(x: rect.width, y: 0))
+        tipPath.close()
+        
+        fillColor.setFill()
+        tipPath.fill()
+    }
+}
---
+++ sample/ViewController.swift
@@ -0,0 +1,194 @@
+import UIKit
+import VietMap
+
+class ViewController: UIViewController {
+    @IBOutlet weak var showMiniMap: UIButton!
+    @IBOutlet weak var zoomIn: UIButton!
+    @IBOutlet weak var zoomOut: UIButton!
+    @IBOutlet weak var showPolygon: UIButton!
+    @IBOutlet weak var useCustomCallOut: UIButton!
+    @IBOutlet weak var longPressHintView: UILabel!
+
+    var mapView: MGLMapView!
+    var coordinates: [CLLocationCoordinate2D] = []
+    var polygon: MGLPolygon?
+    
+    var isShowPolygon: Bool = false
+    var isShowMiniMap: Bool = false
+    var isUseCustomCallOut: Bool = false
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if #available(iOS 10.0, *) {
+            UNUserNotificationCenter.current().requestAuthorization(options: [.badge, .alert, .sound]) { _,_ in
+                DispatchQueue.main.async {
+                    CLLocationManager().requestWhenInUseAuthorization()
+                }
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        startMapView()
+    }
+    
+    
+    func startMapView() {
+        mapView = MGLMapView(frame: view.bounds, styleURL: URL(string: "https://maps.vietmap.vn/api/maps/light/styles.json?apikey=YOUR_API_KEY_HERE"))
+        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        mapView.delegate = self
+        mapView.userTrackingMode = .follow
+        mapView.compassView.compassVisibility = .visible
+
+        let longTap = UILongPressGestureRecognizer(target: self, action: #selector(didLongPress(tap:)))
+        mapView.gestureRecognizers?.filter({ $0 is UILongPressGestureRecognizer }).forEach(longTap.require(toFail:))
+        mapView.addGestureRecognizer(longTap)
+        view.insertSubview(mapView, belowSubview: longPressHintView)
+    }
+    
+    func drawPolyline() {
+        let polyline = MGLPolyline(coordinates: coordinates, count: UInt(coordinates.count))
+        mapView.addAnnotation(polyline)
+    }
+    
+    func drawPolygon() {
+        let coordinates = [
+            CLLocationCoordinate2D(latitude: 10.745863, longitude: 106.655122),
+            CLLocationCoordinate2D(latitude: 10.753557, longitude: 106.649735),
+            CLLocationCoordinate2D(latitude: 10.765662, longitude: 106.681285),
+            CLLocationCoordinate2D(latitude: 10.750961, longitude: 106.683948)
+        ]
+        polygon = MGLPolygon(coordinates: coordinates, count: UInt(coordinates.count))
+        mapView.addAnnotation(polygon!)
+    }
+    
+    // MARK: Gesture Recognizer Handlers
+    @objc func didLongPress(tap: UILongPressGestureRecognizer) {
+        guard let mapView = mapView else { return }
+        if (coordinates.isEmpty) {
+            coordinates.append((mapView.userLocation?.location?.coordinate)!)
+        }
+        let point = mapView.convert(tap.location(in: mapView), toCoordinateFrom: mapView)
+        let annotation = MGLPointAnnotation()
+        annotation.coordinate = point
+        annotation.title = "Point"
+        annotation.subtitle = "Subtitle point"
+        coordinates.append(point)
+        mapView.addAnnotation(annotation)
+        drawPolyline()
+    }
+    
+    // MARK: Action button
+    @IBAction func zoomInPress(_ sender: Any) {
+        let currentZoomLevel = mapView.zoomLevel
+        let newZoomLevel = currentZoomLevel + 1
+        mapView.setZoomLevel(newZoomLevel, animated: true)
+    }
+
+    @IBAction func zoomOutPress(_ sender: Any) {
+        let currentZoomLevel = mapView.zoomLevel
+        let newZoomLevel = currentZoomLevel - 1
+        mapView.setZoomLevel(newZoomLevel, animated: true)
+    }
+
+    @IBAction func showMiniMapPress(_ sender: Any) {
+        if isShowMiniMap {
+            mapView.frame = CGRectMake(0 , 0, self.view.frame.width, self.view.frame.height)
+            mapView.center = view.center
+            showMiniMap.setTitle("Show mini map", for: .normal)
+            isShowMiniMap = false
+        } else {
+            mapView.frame = CGRectMake(0 , 0, self.view.frame.width, self.view.frame.height * 0.5)
+            mapView.center = view.center
+            showMiniMap.setTitle("Show full-screen map", for: .normal)
+            isShowMiniMap = true
+        }
+    }
+    
+    @IBAction func showPolygonPress(_ sender: Any) {
+        if isShowPolygon {
+            guard let polygon = self.polygon else {return}
+            mapView.removeAnnotation(polygon)
+            showPolygon.setTitle("Show polygon", for: .normal)
+            isShowPolygon = false
+        } else {
+            drawPolygon()
+            isShowPolygon = true
+            showPolygon.setTitle("Hide polygon", for: .normal)
+        }
+    }
+
+    @IBAction func useCustomCallOutPress(_ sender: Any) {
+        if isUseCustomCallOut {
+            useCustomCallOut.setTitle("Use custom information view", for: .normal)
+            isUseCustomCallOut = false
+        } else {
+            useCustomCallOut.setTitle("Use default information view", for: .normal)
+            isUseCustomCallOut = true
+        }
+    }
+}
+
+extension ViewController: MGLMapViewDelegate {
+    // MARK: Custom marker
+    func mapView(_ mapView: MGLMapView, imageFor annotation: MGLAnnotation) -> MGLAnnotationImage? {
+        let image = UIImage(systemName: "car")!
+        image.withTintColor(UIColor.red)
+        let annotationImage = MGLAnnotationImage(image: image, reuseIdentifier: "customAnnotation")
+
+        return annotationImage
+    }
+    
+    // MARK: Enable show tooltip with marker
+    func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
+        return true
+    }
+    
+    // MARK: Layout left on tooltip with marker
+    func mapView(_ mapView: MGLMapView, leftCalloutAccessoryViewFor annotation: MGLAnnotation) -> UIView? {
+        let button = UIButton(type: .detailDisclosure)
+        button.addTarget(self, action: #selector(leftButtonTapped(_:)), for: .touchUpInside)
+        button.isUserInteractionEnabled = true
+        return button
+    }
+    
+    @objc func leftButtonTapped(_ sender: UIButton) {
+        print("Left button tapped")
+    }
+
+    // MARK: Layout right on tooltip with marker
+    func mapView(_ mapView: MGLMapView, rightCalloutAccessoryViewFor annotation: MGLAnnotation) -> UIView? {
+        let button = UIButton(type: .close)
+        button.addTarget(self, action: #selector(rightButtonTapped(_:)), for: .touchUpInside)
+        button.isUserInteractionEnabled = true
+        return button
+    }
+    
+    @objc func rightButtonTapped(_ sender: UIButton) {
+        print("right layout")
+    }
+    
+    // MARK: Fill Color in polygon
+    func mapView(_ mapView: MGLMapView, fillColorForPolygonAnnotation annotation: MGLPolygon) -> UIColor {
+        return UIColor.red.withAlphaComponent(0.5)
+    }
+    
+    // MARK: Tap on tooltip
+    func mapView(_ mapView: MGLMapView, tapOnCalloutFor annotation: MGLAnnotation) {
+        print("tap on call out")
+    }
+    
+    // MARK: Custom UIView of tooltip
+    func mapView(_ mapView: MGLMapView, calloutViewFor annotation: MGLAnnotation) -> MGLCalloutView? {
+        if !isUseCustomCallOut {
+            return nil
+        }
+        let calloutView = CustomCalloutView()
+        calloutView.representedObject = annotation
+        return calloutView
+    }
+    
+}
+
